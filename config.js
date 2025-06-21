6fd610c135f63a5b53bd1d18db9151095a3a0edd
jeanbaptistecliff33-u <klzdomina87@gmail.com>
2025-06-21 03:19:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "pur your session id here ",
+SESSION_ID: process.env.SESSION_ID || "IZUKA~MD~RE4xSJaL#w6SLsyFFmqy2yYrpRAMJcHwoNw-BAYv83_WXfncDUmY",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -27,9 +27,9 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/a51qw5.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "IZUKA-MD",
+BOT_NAME: process.env.BOT_NAME || "𓄂⍣⃝𝐆𝚯𝐃✮͢≛⃝𝐑𝐄𝐘✮",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "IZUKA-MD",
+STICKER_NAME: process.env.STICKER_NAME || "𓄂⍣⃝𝐆𝚯𝐃✮͢≛⃝𝐑𝐄𝐘✮",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,10 +37,10 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "50942241547",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "50932341912",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "DAWENS BOY",
-// add bot owner name
+OWNER_NAME: process.env.OWNER_NAME || "𓄂⍣⃝𝐆𝚯𝐃✮͢≛⃝𝐑𝐄𝐘✮",
+//add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ by dawens boy*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/a51qw5.jpeg",
@@ -53,7 +53,7 @@ AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 // make bot public-private-inbox-group 
 ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
 // make anti link true,false for groups 
@@ -65,13 +65,13 @@ AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
 // make false if want private mod
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "50942241547",
+DEV: process.env.DEV || "50932341912",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
@@ -80,5 +80,5 @@ ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
 // make it true for auto recoding
 
-SUDO_NUMBER: process.env.SUDO_NUMBER || "13058962443"    
+SUDO_NUMBER: process.env.SUDO_NUMBER || "18493740033"    
 };
